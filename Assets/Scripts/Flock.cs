4027e7dfed6b6af5bbd47bced9f8fbba7596025b
xyc1993 <m.grygo93@gmail.com>
2020-09-23 01:24:34 +0200

    Copied files from old boids project

---
+++ Assets/Scripts/Flock.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockManager flockManager;
+    private float speed;
+    private bool turning = false;
+
+    private void Start ()
+    {
+        speed = Random.Range(flockManager.minSpeed, flockManager.maxSpeed);
+    }
+
+    private void Update ()
+    {
+        if (flockManager.simpleBehaviour) SimpleUpdate();
+        else FullUpdate();
+    }
+
+    private void SimpleUpdate()
+    {
+        ApplyRules();
+        transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+    }
+
+    private void FullUpdate()
+    {
+        Bounds b = new Bounds(flockManager.transform.position, 2.0f * flockManager.moveLimits);
+        RaycastHit hit = new RaycastHit();
+        Vector3 direction = Vector3.zero;
+
+        if (!b.Contains(transform.position)) //handle getting out of bounds
+        {
+            turning = true;
+            direction = flockManager.transform.position - transform.position;
+        }
+        else if (Physics.Raycast(transform.position, this.transform.forward * flockManager.avoidCollidersDistance, out hit)) //handle collision avoidance
+        {
+            turning = true;
+            direction = Vector3.Reflect(this.transform.forward, hit.normal);
+            if (flockManager.debug) Debug.DrawRay(this.transform.position, this.transform.forward * flockManager.avoidCollidersDistance, Color.red);
+        }
+        else turning = false;
+
+        if (turning) //adjust path if needed
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), flockManager.rotationSpeed * Time.deltaTime);
+        }
+        else //random direction or direction calculated based on the flocking rules
+        {
+            if (Random.Range(0, 100) < 10)
+                speed = Random.Range(flockManager.minSpeed, flockManager.maxSpeed);
+            if (Random.Range(0, 100) < 20)
+                ApplyRules();
+        }
+
+        //move agent forward, along its direction
+        transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+    }
+
+    private void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = flockManager.allAgents;
+
+        Vector3 vcentre = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.01f;
+        float nDistance;
+        int groupSize = 0; //group is a small section of the flock that consists of agents close enough to interact with each other
+
+        foreach (GameObject go in gos)
+        {
+            if(go != this.gameObject)
+            {
+                nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                if(nDistance <= flockManager.neighbourDistance)
+                {
+                    vcentre += go.transform.position;
+                    groupSize++;
+
+                    if(nDistance < flockManager.avoidDistance)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+            }
+        }
+
+        if(groupSize > 0)
+        {
+            vcentre = vcentre/groupSize + (flockManager.goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+            if (direction != Vector3.zero)
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), flockManager.rotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/MoveShape.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShape : MonoBehaviour
+{
+    private Vector3 defaultPosition;
+
+    [Space(10)]
+    [Header("Circle Settings")]
+    public float circleDuration;
+    public float circleRadius;
+    public AnimationCurve circleZ;
+    public AnimationCurve circleY;
+
+    [Space(10)]
+    [Header("Eight Settings")]
+    public float eightDuration;
+    public float eightRadius;
+    public AnimationCurve eightZ;
+    public AnimationCurve eightY;
+
+    private void Start ()
+    {
+        defaultPosition = transform.position;
+    }
+
+    private void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) StartCoroutine(MoveToDefaultPosition(0.5f));
+        if (Input.GetKeyDown(KeyCode.Alpha2)) StartCoroutine(MoveInShapeOnce(circleDuration, circleRadius, circleY, circleZ));
+        if (Input.GetKeyDown(KeyCode.Alpha3)) StartCoroutine(MoveInShapeOnce(eightDuration, eightRadius, eightY, eightZ));
+    }
+
+    public IEnumerator MoveToDefaultPosition(float duration)
+    {
+        Vector3 startPosition = transform.position;
+        float timePassed = 0.0f;
+        while(timePassed < duration)
+        {
+            timePassed += Time.deltaTime;
+            transform.position = Vector3.Lerp(startPosition, defaultPosition, timePassed / duration);
+            yield return null;
+        }
+    }
+
+    //based on Lissajous curve
+    public IEnumerator MoveInShapeOnce(float duration, float shapeRadius, AnimationCurve curveY, AnimationCurve curveZ)
+    {
+        float timePassed = 0.0f;
+        float scale = 1.0f / duration;
+        while (timePassed < duration)
+        {
+            timePassed += Time.deltaTime;
+            transform.position = defaultPosition + new Vector3(0.0f, curveY.Evaluate(timePassed * scale), curveZ.Evaluate(timePassed * scale)) * shapeRadius;
+            yield return null;
+        }
+    }
+}
