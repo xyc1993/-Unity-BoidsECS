b3caaf791e2c47259acb0156ccf8b28682d3bc3a
xyc1993 <m.grygo93@gmail.com>
2020-09-27 03:37:35 +0200

    Refactored boids code to be slightly more optimal and readable

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    [HideInInspector] public BoidsManager boidsManager;
+
+    private float speed;
+    private bool turning = false;
+
+    private void Start ()
+    {
+        speed = Random.Range(boidsManager.minSpeed, boidsManager.maxSpeed);
+    }
+
+    public void SimpleUpdate()
+    {
+        ApplyRules();
+        transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+    }
+
+    public void FullUpdate()
+    {
+        Vector3 direction = Vector3.zero;
+
+        if (!boidsManager.bounds.Contains(transform.position)) //handle getting out of bounds
+        {
+            turning = true;
+            direction = boidsManager.transform.position - transform.position;
+        }
+        else if (Physics.Raycast(transform.position, transform.forward * boidsManager.avoidCollidersDistance, out RaycastHit hit)) //handle collision avoidance
+        {
+            turning = true;
+            direction = Vector3.Reflect(transform.forward, hit.normal);
+            if (boidsManager.debug) Debug.DrawRay(transform.position, transform.forward * boidsManager.avoidCollidersDistance, Color.red);
+        }
+        else turning = false;
+
+        if (turning) //adjust path if needed
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), boidsManager.rotationSpeed * Time.deltaTime);
+        }
+        else //random direction or direction calculated based on the flocking rules
+        {
+            if (Random.Range(0, 100) < 10)
+                speed = Random.Range(boidsManager.minSpeed, boidsManager.maxSpeed);
+            if (Random.Range(0, 100) < 20)
+                ApplyRules();
+        }
+
+        //move agent forward, along its direction
+        transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+    }
+
+    private void ApplyRules()
+    {
+        Vector3 centre = Vector3.zero;
+        Vector3 avoid = Vector3.zero;
+        float groupSpeed = 0.01f;
+        float neighbourDistance;
+        int groupSize = 0; //group is a small section of the flock that consists of agents close enough to interact with each other
+
+        foreach (Boid boid in boidsManager.Boids)
+        {
+            if (boid != this)
+            {
+                neighbourDistance = Vector3.Distance(boid.transform.position, transform.position);
+                if (neighbourDistance <= boidsManager.neighbourDistance)
+                {
+                    groupSize++;
+                    groupSpeed += boid.speed;
+                    centre += boid.transform.position;
+                    if (neighbourDistance < boidsManager.avoidDistance)
+                    {
+                        avoid += (transform.position - boid.transform.position);
+                    }
+                }
+            }
+        }
+
+        if (groupSize > 0)
+        {
+            centre = centre/groupSize + (boidsManager.goalPos - transform.position);
+            speed = groupSpeed / groupSize;
+
+            Vector3 direction = (centre + avoid) - transform.position;
+            if (direction != Vector3.zero)
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), boidsManager.rotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/BoidsManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidsManager : MonoBehaviour
+{
+    [Header("Flock Settings")]
+    public GameObject[] agentsPrefabs;
+    public GameObject goal;
+    public int numAgents = 20;
+    public Vector3 moveLimits = new Vector3(5.0f, 5.0f, 5.0f); //bounds of the flock movement
+    public bool simpleBehaviour = false;
+    public bool debug = true;
+
+    [Header("Agent Settings")]
+    [Range(0.0f, 5.0f)] public float minSpeed;
+    [Range(0.0f, 5.0f)] public float maxSpeed;
+    [Range(1.0f, 10.0f)] public float neighbourDistance;
+    [Range(0.0f, 3.0f)] public float avoidDistance;
+    [Range(0.0f, 5.0f)] public float rotationSpeed;
+    [Range(0.0f, 10.0f)] public float avoidCollidersDistance;
+
+    internal Vector3 goalPos;
+    internal Bounds bounds;
+
+    public Boid[] Boids { get; private set; }
+
+    private void Start ()
+    {
+        Boids = new Boid[numAgents];
+
+        for(int i = 0; i < numAgents; i++)
+        {
+            Vector3 pos = GetRandomPositionWithinMoveLimits();
+            int randomIndex = (int)Random.Range(0, agentsPrefabs.Length);
+            GameObject boidObject = (GameObject)Instantiate(agentsPrefabs[randomIndex], pos, Quaternion.identity, transform);
+            Boid boid = boidObject.GetComponent<Boid>();
+            boid.boidsManager = this;
+            Boids[i] = boid;
+        }
+    }
+
+    private void Update()
+    {
+        UpdateBounds();
+        UpdateGoalPosition();
+        UpdateBoids();
+    }
+
+    private void UpdateBounds()
+    {
+        bounds = new Bounds(transform.position, 2.0f * moveLimits);
+    }
+
+    private void UpdateGoalPosition()
+    {
+        if (goal != null) goalPos = goal.transform.position;
+        else if (Random.Range(0, 100) < 2) goalPos = GetRandomPositionWithinMoveLimits();
+    }
+
+    private void UpdateBoids()
+    {
+        if (simpleBehaviour)
+        {
+            for (int i = 0; i < Boids.Length; i++)
+                Boids[i].SimpleUpdate();
+        }
+        else
+        {
+            for (int i = 0; i < Boids.Length; i++)
+                Boids[i].FullUpdate();
+        }
+    }
+
+    private Vector3 GetRandomPositionWithinMoveLimits()
+    {
+        return transform.position + new Vector3(Random.Range(-moveLimits.x, moveLimits.x),
+                                                Random.Range(-moveLimits.y, moveLimits.y),
+                                                Random.Range(-moveLimits.z, moveLimits.z));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, 2.0f * moveLimits);
+    }
+}
